132d8b0515b5ee75afaeb55cd1658bf45c5498e0
Juan Henao <parjuag@gmail.com>
2020-07-04 14:07:03 -0500

    Authentication Service validate password and generate token

---
+++ sociald-api/src/services/authentication.service.ts
@@ -0,0 +1,56 @@
+import {bind, /* inject, */ BindingScope, service} from '@loopback/core';
+import { repository } from '@loopback/repository';
+import { UserRepository } from '../repositories';
+import { User } from '../models';
+import { CryptingService } from './crypting.service';
+import * as jwt from 'jsonwebtoken';
+
+@bind({scope: BindingScope.TRANSIENT})
+export class AuthenticationService {
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+    @service(CryptingService)
+    public cryptingService: CryptingService
+  ) {}
+
+  async identify(
+    username: string,
+    password: string
+  ):Promise<User | false>
+  {
+    const user = await this.userRepository.findOne({
+        where: {
+          username: username
+        }
+      });
+
+      if(user){
+        const encryptedPassword = this.cryptingService.getDoubledMd5Password(password);
+
+        if(user.password === encryptedPassword){
+          return user;
+        }else{
+          return false;
+        }
+      }
+      return false;
+  }
+
+  async generateToken(user: User){
+    let expiration = Math.floor((Date.now()/1000) * 3600);
+    const jwt_secret_key = "IDONTKNOWYOU";
+    let token = jwt.sign(
+      {
+        exp: expiration,
+        data: {
+          _id: user.id,
+          username: user.username,
+          role: user.role,
+          personId: user.personId
+        }
+      },jwt_secret_key);
+
+    return token;
+  }
+}
