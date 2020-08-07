0e317b54e6063697d444b0057a1eb81165ec8ce7
Juan Henao <parjuag@gmail.com>
2020-08-07 18:33:24 -0500

    Create main services for jwt

--- sociald-api/src/services/index.ts
+++ sociald-api/src/services/index.ts
@@ -1,3 +1,5 @@
 export * from './crypting.service';
 export * from './authentication.service';
 export * from './notification.service';
+export * from './user.service';
+export * from './jwt.service';
