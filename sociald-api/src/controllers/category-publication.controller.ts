29924654be97c3cc26799ff7e38cce977db3b2fe
Juan Henao <parjuag@gmail.com>
2020-08-12 08:16:58 -0500

    Adapt controllers of Category-Publication
    Once change the models and repositories is necessary to
    change controllers too.

--- sociald-api/src/controllers/category-publication.controller.ts
+++ sociald-api/src/controllers/category-publication.controller.ts
@@ -1,10 +1,19 @@
 import {
+  Count,
+  CountSchema,
+  Filter,
   repository,
+  Where,
 } from '@loopback/repository';
 import {
-  param,
+  del,
   get,
   getModelSchemaRef,
+  getWhereSchemaFor,
+  param,
+  patch,
+  post,
+  requestBody,
 } from '@loopback/rest';
 import {
   Category,
@@ -14,14 +23,13 @@ import {CategoryRepository} from '../repositories';
 
 export class CategoryPublicationController {
   constructor(
-    @repository(CategoryRepository)
-    public categoryRepository: CategoryRepository,
+    @repository(CategoryRepository) protected categoryRepository: CategoryRepository,
   ) { }
 
-  @get('/categories/{id}/publication', {
+  @get('/categories/{id}/publications', {
     responses: {
       '200': {
-        description: 'Publication belonging to Category',
+        description: 'Array of Category has many Publication',
         content: {
           'application/json': {
             schema: {type: 'array', items: getModelSchemaRef(Publication)},
@@ -30,9 +38,73 @@ export class CategoryPublicationController {
       },
     },
   })
-  async getPublication(
+  async find(
+    @param.path.string('id') id: string,
+    @param.query.object('filter') filter?: Filter<Publication>,
+  ): Promise<Publication[]> {
+    return this.categoryRepository.publications(id).find(filter);
+  }
+
+  @post('/categories/{id}/publications', {
+    responses: {
+      '200': {
+        description: 'Category model instance',
+        content: {'application/json': {schema: getModelSchemaRef(Publication)}},
+      },
+    },
+  })
+  async create(
     @param.path.string('id') id: typeof Category.prototype.id,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Publication, {
+            title: 'NewPublicationInCategory',
+            exclude: ['id'],
+            optional: ['categoryId']
+          }),
+        },
+      },
+    }) publication: Omit<Publication, 'id'>,
   ): Promise<Publication> {
-    return this.categoryRepository.publication(id);
+    return this.categoryRepository.publications(id).create(publication);
+  }
+
+  @patch('/categories/{id}/publications', {
+    responses: {
+      '200': {
+        description: 'Category.Publication PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async patch(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Publication, {partial: true}),
+        },
+      },
+    })
+    publication: Partial<Publication>,
+    @param.query.object('where', getWhereSchemaFor(Publication)) where?: Where<Publication>,
+  ): Promise<Count> {
+    return this.categoryRepository.publications(id).patch(publication, where);
+  }
+
+  @del('/categories/{id}/publications', {
+    responses: {
+      '200': {
+        description: 'Category.Publication DELETE success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async delete(
+    @param.path.string('id') id: string,
+    @param.query.object('where', getWhereSchemaFor(Publication)) where?: Where<Publication>,
+  ): Promise<Count> {
+    return this.categoryRepository.publications(id).delete(where);
   }
 }
