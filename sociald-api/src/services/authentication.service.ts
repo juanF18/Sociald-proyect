8ec9e58f44a672ef8a5bdee7df22131633af881a
Juan Henao <parjuag@gmail.com>
2020-08-11 20:41:18 -0500

    Delete unnecessary methods

--- sociald-api/src/services/authentication.service.ts
+++ sociald-api/src/services/authentication.service.ts
@@ -19,79 +19,24 @@ export class AuthenticationService {
     public cryptingService: CryptingService,
   ) {}
 
-  async identify(username: string, password: string): Promise<User | false> {
-    /*
-      Gets the user in database and validate credentials
-    */
-    // Try to get the user in the db
-    const user = await this.userRepository.findOne({
-      where: {
-        username: username,
-      },
-    });
+  async ResetPassword(email: string): Promise<string | false> {
+    let user = await this.userRepository.findOne({where: {email: email}});
 
-    // If the user exists then double-crypt the password
-    if (user) {
-      const encryptedPassword = this.cryptingService.getDoubledMd5Password(
-        password,
-      );
-
-      // If the param password is equal to the password in the db return that user
-      if (user.password === encryptedPassword) {
-        return user;
-      } else {
-        return false;
-      }
-    }
-    return false;
-  }
-
-  async generateToken(user: User) {
-    /*
-      Parse the data to tokenize and return it
-    */
-    let expiration = Math.floor((Date.now() / 1000) * 3600);
-    let token = jwt.sign(
-      {
-        exp: expiration,
-        data: {
-          _id: user.id,
-          username: user.username,
-          role: user.role,
-          personId: user.personId,
-        },
-      },
-      jwt_secret_key,
-    );
-
-    return token;
-  }
-
-  async verifyToken(token: string) {
-    /*
-      Try to verify the token passed and return the payload data inside it
-     */
-    try {
-      let data = jwt.verify(token, jwt_secret_key);
-      return data;
-    } catch (Error) {
-      return false;
-    }
-  }
-
-  async ResetPassword(username: string): Promise<string | false> {
-    let user = await this.userRepository.findOne({where: {username: username}});
     if (user) {
       let randomPass = await this.GenerateRandomPassword();
       let encryptPassword = this.cryptingService.getDoubledMd5Password(
         randomPass,
       );
       user.password = encryptPassword;
+
       this.userRepository.replaceById(user.id, user);
+
       return randomPass;
     }
+
     return false;
   }
+
   async GenerateRandomPassword() {
     let randomPass = passGenerator({
       length: passKeys.LENGTH,
@@ -99,6 +44,7 @@ export class AuthenticationService {
       lowercase: passKeys.LOWE_CASE,
       uppercase: passKeys.UPPER_CASE,
     });
+
     return randomPass;
   }
 }
