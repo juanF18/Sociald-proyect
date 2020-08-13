b4bda6c013c786201c9d26ccd1319d24377d9284
Juan-Felipe18 <juanfelipecortescastrillon@gmail.com>
2020-08-13 11:05:34 -0500

    La funcion para mostrar imagenes en las publicaciones esta completa

--- socialdNG/src/app/modules/publication/publication.module.ts
+++ socialdNG/src/app/modules/publication/publication.module.ts
@@ -6,11 +6,15 @@ import { PublicationListComponent } from './publication-list/publication-list.co
 import { PublicationEditionComponent } from './publication-edition/publication-edition.component';
 import { PublicationRemoveComponent } from './publication-remove/publication-remove.component';
 import { PublicationCreationComponent } from './publication-creation/publication-creation.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { FileUploadModule } from 'ng2-file-upload';
+import { CloudinaryModule } from '@cloudinary/angular-5.x';
+import { cloud } from '../../config/cloudinary-config';
+import * as cloudinary from 'cloudinary-core'
 
 
 @NgModule({
@@ -23,6 +27,9 @@ import {MatInputModule} from '@angular/material/input';
     MatChipsModule,
     MatFormFieldModule,
     MatInputModule,
+    FileUploadModule,
+    CloudinaryModule.forRoot(cloudinary, cloud.config),
+    FormsModule,
   ]
 })
 export class PublicationModule { }
